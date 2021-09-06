c53943516f4cea1c23efc7f2b0b1d778e07137dd
krishsam94 <krishsam94@gmail.com>
2021-09-06 14:32:06 +0530

    updated

--- src/Child2.js
+++ src/Child2.js
@@ -2,11 +2,17 @@ import React, { useContext } from "react";
 import { MainContext } from "./context/MainContext";
 
 export default function Child2(props) {
-  const [message, setMessage] = useContext(MainContext);
+  const [state, dispatch] = useContext(MainContext);
   return (
     <>
       <div>Child2</div>
-      <div>{message}</div>
+      <div
+        onClick={() => {
+          dispatch({ type: "ADD" });
+        }}
+      >
+        {state.counter}
+      </div>
     </>
   );
 }
--- src/context/MainProvider.js
+++ src/context/MainProvider.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { MainContext } from "./MainContext";
 
 const { Provider } = MainContext;
 console.log(MainContext);
-export const MainProvider = (props) => {
-  const [message, setMessage] = useState("My name is context");
 
-  return <Provider value={[message, setMessage]}>{props.children}</Provider>;
+let reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    default:
+      return state;
+  }
+};
+export const MainProvider = (props) => {
+  //const [message, setMessage] = useState("My name is context");
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+
+  return <Provider value={[state, dispatch]}>{props.children}</Provider>;
 };
